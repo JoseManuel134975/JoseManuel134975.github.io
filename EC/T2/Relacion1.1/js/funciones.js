dc76798b96197ab16f97b3abecee90ae21fe9950
José Manuel <josemanuel3606@gmail.com>
2024-09-30 18:06:25 +0200

    Prueba

---
+++ EC/T2/Relacion1.1/js/funciones.js
@@ -0,0 +1,14 @@
+// Ejercicio 2: Función
+function sundayBirthday(birth = new Date()) {
+    let year = birth.getFullYear();
+    
+    
+
+    do{
+        
+
+
+
+        year++
+    }while(year <= 2100)
+}
