f05c19b2f983e08af1f007303ef3939c8e7e2a1a
José Manuel <josemanuel3606@gmail.com>
2024-10-02 16:22:08 +0200

    Relacion 1.2 completada :)

--- EC/T2/Relacion1.2/js/funciones.js
+++ EC/T2/Relacion1.2/js/funciones.js
@@ -1,12 +1,31 @@
-
+// Ejercicio 1
+let cont = 60 // No hacen falta variables globales porque JavaScript las detecta ignorando el orden del código :)
 function contadorAtras() {
-    document.write(contador + "<br>"); // Muestra el valor del contador
-    if (contador > 0) {
-        contador--; // Decrementa el contador
-    } else {
-        clearInterval(intervalo); // Detiene el intervalo cuando llega a 0
-        document.write("Fin de la cuenta atrás");
+    if(cont > 0){
+        document.write(cont + "<br>")
+        cont--
+    }else{
+        clearInterval(timerId) // Detiene el intervalo
+        document.write("Se acabo el tiempo")
     }
+
 }
 
-let intervalo = setInterval(contadorAtras, 1000); // Pasa la referencia de la función, sin comillas
+
+// Ejercicio 2
+function reloj() {
+    let now = new Date()
+    let hour = now.getHours()
+    let minute = now.getMinutes()
+    let second = now.getSeconds()
+    document.getElementById("reloj").innerHTML = hour + ":" + minute + ":" + second // Imprime el reloj sin mostrarlo repetidamente
+    
+    return second // Necesario para el ejercicio 3*********
+}
+
+// Ejercicio 3
+function relojGoogle() {
+    if(reloj() === 0){
+        window.location.assign("https://www.google.com/") // Cambia la URL
+    }
+}
