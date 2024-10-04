592a4cba051293569326c2de7818ea48fbe4efff
José Manuel <josemanuel3606@gmail.com>
2024-10-04 15:23:16 +0200

    Relacion de matemáticas 'hecha'

--- EC/T2/Relacion1.3(math)/js/funciones.js
+++ EC/T2/Relacion1.3(math)/js/funciones.js
@@ -23,3 +23,29 @@ function anguloCalculos(ang){
 function hipotenusa(cateto1, cateto2){
     return Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2))
 }
+
+function mostrarImagen(){
+    window.onload = function() {
+        // Crear un elemento <img>
+        var img = document.createElement('img');
+        var img2 = document.createElement('img');
+        var img3 = document.createElement('img');
+        // Y las metemos dentro de un array para que Math.random() funcione
+        const container = [img, img2, img3];
+        let random = 0
+        
+        // Asignar la fuente de la imagen
+        img.src = '../img/fall-4589724_1280.jpg'; // Cambia a la ruta de tu imagen
+        img2.src = '../img/pumpkins-8338100_1280.jpg'; // Cambia a la ruta de tu imagen
+        img3.src = '../img/sea-7914544_1280.jpg'; // Cambia a la ruta de tu imagen
+        
+        // Opcional: Añadir atributos adicionales como el 'alt' o estilos
+        img.style.width = '300px'; // Cambiar el tamaño si es necesario
+        img2.style.width = '300px'; 
+        img3.style.width = '300px'; 
+        
+        // Agregar la imagen al contenedor y generamos el número aleatorio
+        random = Math.floor(Math.random()*3)
+        document.getElementById('img').appendChild(container[random]); // Mostramos
+    }
+}
--- EC/T2/Relacion1.3(strings)/js/funciones.js
+++ EC/T2/Relacion1.3(strings)/js/funciones.js
@@ -2,16 +2,15 @@ function invierteCadena(cad_arg){
     return cad_arg.split("").reverse().join(""); // Visual Studio no detecta los métodos de String porque en JavaScript no se pasan los parámetros con su tipo correspondiente (creo)
 }
 
-// split para separar con delimitadores las palabras
+// split para convertir el string en un array de palabras y manejarlas mejor
 // reverse para invertir el orden de las palabras
-// join para unir las palabras (por si acaso)
+// join para unirlo todo
 function inviertePalabras(cad_arg){
     return cad_arg.split(" ").reverse().join(" "); 
 }
 
-// sort para ordenar las palabras de mayor a menor
-// Si b es mayor que a se ordena de forma descendente (y viceversa)
-// [0] se devuelve el primer elemento porque lo ordena de mayor a menor 
+// sort para ordenar las palabras de mayor a menor 
+// se resta la longitud de la palabra 
 function encuentraPalabraMasLarga(cad_arg){
     return cad_arg.split(" ").sort((a, b) => b.length - a.length)[0];
 }
@@ -42,8 +41,18 @@ function insideSubstring(cad_arg, sub_arg){
 
 function changeString(cad_arg){
     // Con match buscamos las vocales y consonantes pero nos devuelve una array*
-    const consonantes = cad_arg.match(/[^aeiouAEIOU]/g)
+    const consonantes = cad_arg.match(/[^aeiouAEIOU\s]/g) // Con el g nos aseguramos de que se buscan todas las vocales y con s que se excluyen los espacios
     const vocales = cad_arg.match(/[aeiouAEIOU]/g)
 
     return consonantes.join('') + vocales.join('') // Con join uno las arrays en una cadena (sin espacios)
+}
+
+function deleteCharRepeated(cad_arg){
+    // Con filter quitamos los caracteres repetidos
+    // En estos casos he usado function arrow para que sea más legible y rápido****************
+    // Se le pasa a filter los parámetros de manera que:
+    // indexOf(c) nos devuelve el primer indice del caracter que haya en esa posicion
+    // Y se comprueba si es igual a la posicion actual (index)
+    // Resumidamente, como un bucle for
+    return cad_arg.split("").filter((c, index) => cad_arg.indexOf(c) === index).join("");
 }
