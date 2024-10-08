9317d7cce63122b5cf8a8c49a7a29abe1b82f383
José Manuel <josemanuel3606@gmail.com>
2024-10-08 17:38:08 +0200

    Relacion 1.6 terminada...

--- EC/T3/Relacion1.6/js/funciones.js
+++ EC/T3/Relacion1.6/js/funciones.js
@@ -47,24 +47,113 @@ function ejercicio2() {
     document.getElementById("availLeft").textContent = userScreen
 }
 
-var ventanaGeneral
-var ventana
-
-function windowConfirm(id) { // Le pasamos el id del elemento (con this.id desde HTML)
-    const item = document.getElementById(id); // Obtenemos el elemento dentro del objeto para poder acceder a su id y mostrarlo
-
-    // Asignar el evento onclick al elemento clicado
-    // Abrir una nueva ventanaGeneral y mostrar el id del elemento clicado
-    ventanaGeneral = window.open("", "_blank");
-    ventanaGeneral.document.write("<h1>Has pulsado la opción número " + item.id + "</h1>");
+function openConfirmWindow() {
+    let ventana = window.open("", "_blank", "width=500,height=400");
+    ventana.document.write("<h1>Confirm</h1>");
+    ventana.document.write("<br> <button onclick='window.close();'>Close</button>");
 }
 
-function checkWindow(){
-    ventana = window.open("", "_blank");
-
-    if (ventana.closed) {
-        window.document.getElementById("checkWindow").innerHTML = "La ventanaGeneral ha cerrado"
+function openWindowAndCheckIfClosed() {
+    let ventana = window.open("", "_blank", "width=500,height=400");
+    ventana.document.write("<h1>Confirm</h1>");
+    if(!ventana.closed){
+        document.getElementById("checkWindow").innerHTML = "La ventana esta abierta";
     }else{
-        window.document.getElementById("checkWindow").innerHTML = "La ventanaGeneral no ha cerrado"
+        document.getElementById("checkWindow").innerHTML = "La ventana se ha cerrado";
     }
+}
+
+function openWindowAndPutNewName() {
+    let ventana = window.open("", "_blank", "width=500,height=400");
+    ventana.document.write("<h1>Confirm</h1>");
+    let newName = ventana.prompt("Introduce el nuevo nombre");
+    ventana.name = newName
+    document.getElementById("newName").innerHTML = "El nuevo nombre es: " + ventana.name
+}
+
+function openWindowWithSize() {
+    let ventana = window.open("", "_blank", "width=300,height=100");
+    ventana.document.write("<h1>Confirm</h1>");
+    let ancho = ventana.prompt("Introduce el ancho");
+    let alto = ventana.prompt("Introduce el alto");
+    ventana.resizeTo(ancho, alto)
+    document.getElementById("newSize").innerHTML = "El nuevo tamaño es: " + ventana.outerWidth + "x" + ventana.outerHeight
+    ventana.moveTo(50, 50)
+    let x = ventana.prompt("Introduce la coordenada x");
+    let y = ventana.prompt("Introduce la coordenada y");
+    ventana.moveBy(x, y)
+}
+
+function moveDisplacementBar() {
+    window.scrollBy(0, 10);
+    document.getElementById("displacementBar").innerHTML = "La barra de desplazamiento se ha desplazado " + window.scrollY + "px";
+    let x = window.prompt("Introduce la coordenada x");
+    let y = window.prompt("Introduce la coordenada y");
+    window.scrollTo(x, y);
+    document.getElementById("displacementBar").innerHTML = "La barra de desplazamiento se ha desplazado " + window.scrollY + "px" + " y se ha movido a las coordenadas (" + x + ", " + y + ")";
+}
+
+function goToMarker() {
+    const item = document.getElementById("marcador");
+
+    window.location.hash = "marcador";
+    document.getElementById("marcador").innerHTML = "Hola, soy un marcador...";
+}
+
+function getHostname() {
+    document.getElementById("hostname").innerHTML = window.location.hostname;
+}
+
+function getURL() {
+    document.getElementById("url").innerHTML = window.location.href;
+}
+
+function goToURL() {
+    let url = prompt("Introduce la URL a redirigir:");
+    window.location.href = url;
+}
+
+function getProtocol() {
+    document.getElementById("protocol").innerHTML = window.location.protocol;
+}
+
+function reloadPage() {
+    window.location.reload();
+}
+
+function getLastUpdate() {
+    document.getElementById("lastUpdate").innerHTML = document.lastModified;
+}
+
+function getCurrentURL() {
+    document.getElementById("currentURL").innerHTML = document.referrer;
+}
+
+function getTitle() {
+    document.getElementById("title").innerHTML = document.title;
+}
+
+function getURL() {
+    document.getElementById("url").innerHTML = document.URL;
+}
+
+function getNumberOfAnchors() {
+    document.getElementById("anchors").innerHTML = document.anchors.length;
+}
+
+function getNumberOfImages() {
+    document.getElementById("images").innerHTML = document.images.length;
+}
+
+function getIDofFirstImage() {
+    document.getElementById("firstImage").innerHTML = document.images[0].id;
+}
+
+function getNumberOfLinks() {
+    document.getElementById("links").innerHTML = document.links.length;
+}
+
+function changeTitle() {
+    let newTitle = prompt("Introduce el nuevo tiútulo:");
+    document.title = newTitle;
 }
