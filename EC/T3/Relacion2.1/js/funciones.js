87d3f4c56a10ea38189b16c4c996b2114fbf47b7
José Manuel <josemanuel3606@gmail.com>
2024-11-04 21:14:30 +0100

    Sergio ++

--- EC/T3/Relacion2.1/js/funciones.js
+++ EC/T3/Relacion2.1/js/funciones.js
@@ -73,3 +73,124 @@ const divisibleByThree = (array) => {
 
     return check
 }
+
+const zipArrays = (array1, array2) => concactA = array1.concat(array2)
+
+const sortJoinedArray = (array) => array.sort((a, b) => a - b)
+
+const deleteFirstWord = (array) => {
+    array.splice(0, 1)
+
+    return array
+}
+
+const newWordAtTheStart = (array) => {
+    array.splice(0, 0, "Jamas")
+
+    return array
+}
+
+const replaceSomeElements = (array) => {
+    array.splice(2, 1, "Nunca")
+
+    return array
+}
+
+const findInArray = (namesArray) => {
+    const startWithJ = []
+    const initials = []
+
+    namesArray.forEach(element => {
+        if (element.charAt(0) === 'J') {
+            startWithJ.push(element)
+        }
+    })
+
+    // console.log(startWithJ)
+    namesArray.forEach(element => {
+        initials.push(element.charAt(0))
+    })
+
+    initials.sort()
+    const concat = startWithJ.concat(initials)
+
+    return concat
+}
+
+const treasureHunt = () => {
+    let treasure = 52
+    let fila = 0
+    let columna = 0
+
+    const map = [
+        [34, 21, 32, 41, 25],
+        [14, 42, 43, 14, 31],
+        [54, 45, 52, 42, 23],
+        [33, 15, 51, 31, 35],
+        [21, 52, 33, 13, 23]
+    ]
+
+    while (true) {
+        let valor = map[fila][columna]
+        console.log(`Fila: ${fila} - Columna: ${columna} - Valor: ${valor}`)
+
+        if (valor === treasure) {
+            console.log(`¡Has encontrado el tesoro!`)
+            break
+        } else {
+            map[fila].splice(columna, 1)
+            console.log(map)
+        }
+
+        // if (fila < 0 || fila >= 5 || columna < 0 || columna >= map[fila].length) {
+        //     fila++
+        // }
+
+        fila = Math.floor(valor / 10) - 1;
+        columna = (valor % 10) - 1;
+
+        fila = (fila + map.length) % map.length;
+        columna = (columna + map[0].length) % map[0].length;
+    }
+}
+
+const treasureHuntV2 = (filas, columnas) => {
+    let treasure = 52
+    let fila = 0
+    let columna = 0
+
+    const map = []
+
+    for (let i = 0; i < filas; i++) {
+        let fila = []
+        for (let j = 0; j < columnas; j++) {
+            fila.push(Math.floor(Math.random() * 51) + 11)
+        }
+        map.push(fila)
+    }
+
+    map[4][1] = 52
+    
+    while (true) {
+        let valor = map[fila][columna]
+        console.log(`Fila: ${fila} - Columna: ${columna} - Valor: ${valor}`)
+
+        if (valor === treasure) {
+            console.log(`¡Has encontrado el tesoro!`)
+            break
+        } else {
+            map[fila].splice(columna, 1)
+            console.log(map)
+        }
+
+        // if (fila < 0 || fila >= 5 || columna < 0 || columna >= map[fila].length) {
+        //     fila++
+        // }
+
+        nuevafila = Math.floor(valor / 10) - 1;
+        nuevacolumna = (valor % 10) - 1;
+
+        fila = (nuevafila + map.length) % map.length;
+        columna = (nuevacolumna + map[0].length) % map[0].length;
+    }
+}
